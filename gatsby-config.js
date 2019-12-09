15f905ab8285c6aea4eb33fa08cd3de2a241b3b0
Josh Cox <josh@webhosting.coop>
2019-12-09 10:05:13 -0600

    Favicon and matching reds

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663333`,
         theme_color: `#663333`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/NAlocal512.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#FF1111`,
+      background: `#FA0c0c`,
       marginBottom: `1.45rem`,
     }}
   >
