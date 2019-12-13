ef7bf9ff9e8992b3d49437e0809df821aec5b9d0
Josh Cox <josh@webhosting.coop>
2019-12-12 19:52:08 -0600

    Intro-to-Git

--- src/pages/benify.js
+++ src/pages/benify.js
@@ -11,9 +11,9 @@ const BenifyPage = () => (
     <p>Here we'd like to include some useful resources for anyone who would like to become a dude named ben.</p>
     <p>Or just anyone who would like to sharpen some of the necessary skills to contribute to this website.</p>
     <p><h2>Git</h2></p>
-    <p>First complete my crash course <a href="https://www.katacoda.com/thoth/scenarios/introtogit">intro to git</a>.</p>
+    <p>First complete my crash course <a href="https://www.katacoda.com/thoth/scenarios/introtogit">Intro-to-Git</a>.</p>
     <p>Then you should become a <a href="https://github.com/GitCertifiedCollaborator/PullRequestCertification">Git Certified Collaborator</a>.</p>
-    <p>Those on this <a href="https://github.com/GitCertifiedCollaborator/PullRequestCertification/blob/master/certified.md">list</a> are certified to not be nincompoops. Or at least have the bare minimum set of skills necessary to collaborate with other human beings via git.</p>
+    <p>Those on this <a href="https://github.com/GitCertifiedCollaborator/PullRequestCertification/blob/master/certified.md">list</a> are certified to not be nincompoops... Or at least have the bare minimum set of skills necessary to collaborate with other human beings via git.</p>
     <p>There is also our <a href="https://nalocal512.gitlab.io/">gitlab portal</a>. Where you can find all of our repos.</p>
     <p><h2>Gatsby</h2></p>
     <p>Gatsby has their own set of excellent <a href="https://www.gatsbyjs.org/tutorial/">tutorials</a>.</p>
