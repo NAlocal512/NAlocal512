457b6b7e03d3faa7ba094e10bd1d8743010c482f
Josh Cox <josh@webhosting.coop>
2019-12-12 19:45:31 -0600

    More about

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,9 +9,11 @@ const AboutPage = () => (
   <Layout>
     <SEO title="About" />
     <p>This site was built by local No Agenda fans from the NAlocal512 meetup group.</p>
-    <p>
-      To help with the development of this site please visit our <a href="https://nalocal512.gitlab.io/"> gitlab portal page</a>.
-    </p>
+    <p>Our websites are built in <a href="https://gatsbyjs.org/">Gatsby</a> a web framework built to make blazing fast static websites from a template framework that allows you to use react components.</p>
+    <p>The fantastic CI/CD available here at <a href="https://gitlab.com/">Gitlab</a> reads the <code>.gitlab-ci.yml</code> performs a build and test of our site.  </p>
+    <p>And then publishes the rendered static site to the CDN, which is proxied by <a href="https://cloudflare.com/">Cloudflare</a>, to give front-end sites that are wicked fast.</p>
+    <p>Then any dynamic pieces that require a database backend or other server side processing, like the shop,  mailing list and domain management are conducted by the <a href="https://webhosting.coop/">Webhosting.coop</a>.</p>
+    <p>If you would like to learn to become a dude named ben who helps with our web sites try starting on this <Link to="/benify/">page</Link> to find a listing of the top resources we think might help you get started.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
